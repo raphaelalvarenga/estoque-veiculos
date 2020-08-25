0a36b47dcdcdf1ac38554b27dc01321e9f6b168e
Raphael Alvarenga <raphael.carmo@eurotextil.com.br>
2020-08-25 10:13:32 -0300

    [Server] Criado o arquivo App que funcionara como uma classe para o servidor. Todas as configuracoes foram colocadas como cors, express, bodyParser, e rotas. A medida que a aplicacao for criada, esta classe sera implementada. No index, o servidor ja esta instanciado e criando o servidor. Instalado tambem o ts-node para que o nodemon rode os arquivos TypeScript.

---
+++ src/classes/App.ts
@@ -0,0 +1,31 @@
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+export default class App {
+
+    // Esta variável vai ser responsável por todo o trabalho do express
+    private server: Application;
+
+    // Quando o servidor inicia, ele precisa configurar porta, cors, rotas, etc
+    constructor(private port?: number | string) {
+        this.server = express();
+        this.settings();
+        this.routes();
+    }
+
+    public settings() {
+        this.server.use(bodyParser.urlencoded({ extended: false }));
+        this.server.use(bodyParser.json());
+        this.server.use(cors());
+        this.server.set("port", process.env.PORT || this.port || 3000);
+    }
+
+    public routes() {
+
+    }
+
+    public listen() {
+        this.server.listen(this.server.get("port"), () => console.log(`Port ${this.server.get("port")} running...`));
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import App from "./classes/App";
+
+// Esta é uma instância do servidor. Sinta-se à vontade para mudar a porta passando-a como parâmetro
+const app = new App();
+
+// Este comando lança o servidor
+app.listen();
