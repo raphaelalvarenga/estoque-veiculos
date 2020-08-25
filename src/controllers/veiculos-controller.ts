f68899cdebefcc75c0a0c93569b77069a0fe8cdd
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2020-08-25 20:59:05 -0300

    [Rotas e Controllers] As rotas e controladores foram reformulados. A rota agora apenas encaminha o fluxo do codigo para o controlador. O controlador agora ficara responsavel pela regra de negocio.

--- src/controllers/veiculos-controller.ts
+++ src/controllers/veiculos-controller.ts
@@ -1,7 +1,37 @@
 import veiculosModel from "../models/veiculos-model";
+import { Request, Response } from "express";
+import Veiculo from "../interfaces/veiculo-interface";
 
-const veiculosController = async () => {
-    return await veiculosModel();
+const veiculosController = {
+    getVeiculos: async (req: Request, res: Response) => {
+        const veiculos: Veiculo[] = await veiculosModel() as Veiculo[];
+        res.json(veiculos);
+    },
+
+    getVeiculoById: async (req: Request, res: Response) => {
+        console.log(`Buscar veículo com o id ${req.params.id}`);
+        res.json({rota: `/veiculos/${req.params.id}`});
+    },
+
+    insertVeiculo: async (req: Request, res: Response) => {
+        console.log("Cadastrar veículo");
+        res.json({rota: `/veiculos (cadastrar)`});
+    },
+
+    putVeiculo: async (req: Request, res: Response) => {
+        console.log(`Atualizar o veículo com id ${req.params.id}`);
+        res.json({rota: `/veiculos/${req.params.id} (atualizar)`});
+    },
+
+    patchVeiculo: async (req: Request, res: Response) => {
+        console.log(`Atualizar parcialmente o veículo ${req.params.id}`);
+        res.json({rota: `/veiculos/${req.params.id} (atualizar parcial)`});
+    },
+
+    deleteVeiculo: async (req: Request, res: Response) => {
+        console.log(`Deletar o veículo de id ${req.params.id}`);
+        res.json({rota: `/veiculos/${req.params.id} (deletar)`});
+    }
 }
 
 export default veiculosController;
--- src/routes.ts
+++ src/routes.ts
@@ -3,39 +3,18 @@ import veiculosController from "./controllers/veiculos-controller";
 
 const router = Router();
 
-router.route("/veiculos")
-    .get(async (req: Request, res: Response) => {
-        const veiculos = await veiculosController();
-        res.json(veiculos);
-    });
+const { getVeiculos, getVeiculoById, insertVeiculo, putVeiculo, patchVeiculo, deleteVeiculo } = veiculosController;
 
-router.route("/veiculos/:id")
-    .get((req: Request, res: Response) => {
-        console.log(`Buscar veículo com o id ${req.params.id}`);
-        res.json({rota: `/veiculos/${req.params.id}`});
-    });
+router.route("/veiculos").get(getVeiculos);
 
-router.route("/veiculos")
-    .post((req: Request, res: Response) => {
-        console.log("Cadastrar veículo");
-        res.json({rota: `/veiculos (cadastrar)`});
-    });
+router.route("/veiculos/:id").get(getVeiculoById);
 
-router.route("/veiculos/:id")
-    .put((req: Request, res: Response) => {
-        console.log(`Atualizar o veículo com id ${req.params.id}`);
-        res.json({rota: `/veiculos/${req.params.id} (atualizar)`});
-    });
+router.route("/veiculos").post(insertVeiculo);
 
-router.route("/veiculos/:id")
-    .patch((req: Request, res: Response) => {
-        console.log(`Atualizar parcialmente o veículo ${req.params.id}`);
-        res.json({rota: `/veiculos/${req.params.id} (atualizar parcial)`});
-    });
+router.route("/veiculos/:id").put(putVeiculo);
 
-router.route("/veiculos/:id")
-    .delete((req: Request, res: Response) => {
-        console.log(`Deletar o veículo de id ${req.params.id}`);
-        res.json({rota: `/veiculos/${req.params.id} (deletar)`});
-    });
+router.route("/veiculos/:id").patch(patchVeiculo);
+
+router.route("/veiculos/:id").delete(deleteVeiculo);
+
 export default router;
