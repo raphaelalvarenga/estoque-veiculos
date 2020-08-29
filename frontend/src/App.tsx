86f70cd682fba44d1ad32bf93c16a82d245ce671
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2020-08-29 03:49:41 -0300

    [App Bar e Drawer] Criados o AppBar e Drawer. Ainda nao foi criada a navegacao entre rotas.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./views/Home";
 import CadastrarVeiculo from "./views/CadastrarVeiculo";
-import { CssBaseline } from "@material-ui/core";
+import { CssBaseline, AppBar, Toolbar, IconButton, Typography, Button, Drawer, List, ListItem, ListItemText, ListItemIcon } from "@material-ui/core";
 import { Switch, Route, BrowserRouter, Link } from "react-router-dom";
+import { Menu, Person, Home as HomeIcon, DirectionsCar } from "@material-ui/icons";
+import globalStyles from "./assets/styles/styles";
 
 const App: React.FunctionComponent = () => {
+
+    const classes = globalStyles();
+
+    const [drawer, setDrawer] = useState<boolean>(false);
+
     return (
         <BrowserRouter>
             <CssBaseline />
+
+            <AppBar position = "static">
+                <Toolbar>
+                    <IconButton
+                        edge = "start"
+                        color = "inherit"
+                        className = {classes.menuButton}
+                        onClick = {() => setDrawer(true)}
+                    >
+                        <Menu />
+                    </IconButton>
+
+                    <Typography variant = "h6" className = {classes.title}></Typography>
+
+                    <Button color = "inherit"><Person /></Button>
+                </Toolbar>
+            </AppBar>
+
+            <Drawer
+                anchor = "left"
+                open = {drawer}
+                onClose = {() => setDrawer(false)}
+            >
+                <List className = {classes.drawerList}>
+                    {
+                        [
+                            {nome: "Home", link: "/"},
+                            {nome: "Cadastrar Veículo", link: "/cadastrar-veiculo"}
+                        ].map(rota => (
+                            <ListItem button>
+                                <ListItemIcon>
+                                    {
+                                        rota.nome === "Home" ? <HomeIcon /> : <DirectionsCar />
+                                    }
+                                </ListItemIcon>
+                                <ListItemText primary = {rota.nome} />
+                            </ListItem>
+                        ))
+                    }
+                </List>
+            </Drawer>
+
+
 
             <nav>
                 <ul>
--- frontend/src/assets/styles/styles.ts
+++ frontend/src/assets/styles/styles.ts
@@ -2,6 +2,22 @@ import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 
 const globalStyles = makeStyles((theme: Theme) =>
     createStyles({
+        root: {
+            flexGrow: 1,
+        },
+
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+
+        title: {
+            flexGrow: 1,
+        },
+
+        drawerList: {
+            width: "250px"
+        },
+
         paper: {
             maxWidth: "1200px",
             margin: "auto",
